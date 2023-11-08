4d05e79ebb8fe2126805b8ec68b129b2d9c2801c
vitupl <victorhgogo@gmail.com>
2023-11-08 14:22:20 -0300

    ejs,rederHTML,viewFunc

--- Scripts/server.js
+++ Scripts/server.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs")
+
 app.get('/', (req, res)=>{
     console.log("aqui")
-    res.send("ola e bem vindo ao meu site")
+    res.render('index', { port : port})
+})
+app.get('/sobre', (req, res)=>{
+    //mostrar nome do criador e afins
+    res.send("esta pagina esta em construção")
+   
+})
+app.get('/google', (req, res)=>{
+    //clone do google(demems)
+    res.send("google.com")
 })
 app.listen(port) //http://localhost:port
 
