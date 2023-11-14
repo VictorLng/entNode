b1cc7a1a8c9ae34c9177ed8d98ea2a13c8904a75
vitupl <victorhgogo@gmail.com>
2023-11-14 14:06:03 -0300

    Re-estilização

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,16 @@
 let div = document.getElementById("allChamps")
 let btn = document.getElementById("btn")
-function getChamp(champ){
-  axios.get(`https://ddragon.leagueoflegends.com/cdn/13.22.1/data/pt_BR/champion/${champ}.json`)
+let champName = document.getElementById("buscarProduto")
+function getChamp(champ, x){
+  axios.get(`https://ddragon.leagueoflegends.com/cdn/13.22.1/data/pt_BR/champion.json`)
   .then(response=>{
-    console.log(response.data.data)
+    champImage.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champ}_${x}.jpg`
+    console.log(response.data.data[`${champ}`])
   })
   .catch(error => console.log(error))
 }
 
+
 btn.addEventListener("click", ()=>{
-  getChamp("Aatrox")
+  getChamp(champName.value.charAt(0).toUpperCase() + champName.value.slice(1).toLowerCase(), 0)
 })
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+const { default: axios } = require('axios');
 const express = require('express')
 const path = require('path');
 const app = express()
 const port = 3000
 
-
 app.use(express.static(path.join(__dirname,'public')))
-
 app.set("view engine", "ejs")
 
+// function champElements(){
+//     axios.get()
+// }
 
 app.get('/', (req, res)=>{
    var blink =  "bom dia"
